82d22696c1269a3cfcb79a1a5ce4fff923fd8e3c
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 21:49:00 -0800

    fix licenses

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -o=${PREFIX}/bin/ejson-kms -ldflags="-s -w"
-go-licenses save . --save_path=license-files
+go-licenses save . --save_path=license-files --ignore github.com/jmespath/go-jmespath
+
+# Manually copy licenses that go-licenses could not download
+cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
